b45d2407e3ffd50f0d182c458f11905439940f0d
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-10-24 12:37:06 +0200

    gmock not needed

--- clang_repro/main.cpp
+++ clang_repro/main.cpp
@@ -1,10 +1,9 @@
 #include <cstdio>
-#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char* argv[])
 {
-    testing::InitGoogleMock(&argc, argv);
+    testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
