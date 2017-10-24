8f21c152c18be599dde8787b01f3c180ac1348c4
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-10-24 13:19:04 +0200

    macros unraveled

--- clang_repro/Tests.cpp
+++ clang_repro/Tests.cpp
@@ -4,13 +4,24 @@
 
 #include "A.hpp"
 
-class ATest : public testing::Test
+struct ATest : public testing::Test
 {
-public:
     A x;
-
 };
 
-TEST_F(ATest, TheTest)
+class ATest_TheTest_Test : public ATest
 {
-}
+public:
+    ATest_TheTest_Test() {}
+private:
+    virtual void TestBody();
+    static ::testing::TestInfo* const test_info_ __attribute__((unused));
+    ATest_TheTest_Test(ATest_TheTest_Test const&);
+    void operator=(ATest_TheTest_Test const&);
+};
+::testing::TestInfo* const ATest_TheTest_Test::test_info_ =
+    ::testing::internal::MakeAndRegisterTestInfo(
+        "ATest", "TheTest", __null, __null, (::testing::internal::GetTypeId<ATest>()),
+        ATest::SetUpTestCase, ATest::TearDownTestCase,
+        new ::testing::internal::TestFactoryImpl<ATest_TheTest_Test>);
+void ATest_TheTest_Test::TestBody() {}
